17311592908f98c775027719337f6081fa1b7186
Patti Jurgensen <patricia.jurgensen@gmail.com>
2014-04-15 11:20:51 -0700

    setup style

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :tweets
   has_many :follows
 
+  valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :handle, presence: true
-  validates :email, presence: true
-  validates :password_digest, presence: true
+  validates :email,
+            presence: true,
+            format: { with: valid_email_regex },
+            uniqueness: true,
+            case_sensitive: false
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_digest, presence: true, uniqueness: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,9 @@ describe User do
 
   it { should validate_presence_of :handle }
   it { should validate_presence_of :email }
+  it { should allow_value('example@example.org', 'another.example@example.org').for(:email)}
+  it { should validate_uniqueness_of :email }
   it { should validate_presence_of :password_digest }
+  it { should ensure_length_of(:password).is_at_least(6) }
 
 end
