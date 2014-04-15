4f124a86bb99b7d82020c9f5300a1e3c28e5e697
Patti Jurgensen <patricia.jurgensen@gmail.com>
2014-04-15 11:22:38 -0700

    update User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,8 @@ describe User do
   it { should validate_presence_of :email }
   it { should allow_value('example@example.org', 'another.example@example.org').for(:email)}
   it { should validate_uniqueness_of :email }
-  it { should validate_presence_of :password_digest }
   it { should ensure_length_of(:password).is_at_least(6) }
+  it { should validate_presence_of :password_digest }
+  it { should validate_uniqueness_of :password_digest }
 
 end
